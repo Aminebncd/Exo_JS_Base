f519a55025816be956757633e39a720c8da4dffb
aminebncd <amine.bounachada@hotmail.com>
2024-01-18 16:33:33 +0100

    debut exo 2

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ alert (`class : carre
 - display : ${style.display}`)
 
 }
+
+const vassal1 = document.getElementById("carreUn");
+const vassal2 = document.getElementById("carreDeux");
+const vassal3 = document.getElementById("carreTrois");
+const vassal4 = document.getElementById("carreQuatre");
+
+const boss = document.getElementById("carreBoss");
+
+vassal1.addEventListener('click', changeColor(vassal1));
+vassal2.addEventListener('click', changeColor(vassal2));
+vassal3.addEventListener('click', changeColor(vassal3));
+vassal4.addEventListener('click', changeColor(vassal4));
+
+changeColor(vassal){
+    boss.style.backgroundColor = vassal.style.backgroundColor;
+}
+
+
+// boss.style.backgroundColor = 
